ab724c017fb7886b8fe7cac5df9f153f590db1f4
Brad <bbergstrum@hotmail.com>
2019-06-08 01:38:16 +1000

    Added single ticket endpoint and comments

--- routes/tickets.js
+++ routes/tickets.js
@@ -11,13 +11,12 @@ var zendesk = new Zendesk({
   token: process.env.ZENDESK_TOKEN
 });
 
-
 /* GET landing page */
-
 router.get('/', function(req, res){
   res.redirect('/tickets');
 });
 
+// GET entire ticket collection
 router.get('/tickets', function(req, res) {
   res.setHeader('Content-Type', 'application/json');
   //grab all tickets from Zendesk API
@@ -27,10 +26,24 @@ router.get('/tickets', function(req, res) {
   }).catch(console.error);
 });
 
+// GET single ticket
+router.get('/ticket/:ticketId', function(req, res) {
+   // url params return as string? - req.params from front end query
+  const ticketId = parseInt(req.params.ticketId)
+  res.setHeader('Content-Type', 'application/json');
+  zendesk.tickets.show(ticketId).then(function(ticket){
+    res.end(JSON.stringify(ticket));
+  }).catch(console.error);
+});
+
+// GET tickets per page
 router.get('/ticketsByPage/:pageId', function(req, res){
+  // url params return as string? - parse req.params from front end query
   const pageNum = parseInt(req.params.pageId) - 1;
   const itemPerPage = 25;
+  // first ticket
   const firstIndex = itemPerPage * pageNum;
+  // last ticket
   const lastIndex = (pageNum + 1) * itemPerPage;
   res.setHeader('Content-Type', 'application/json');
   //grab all tickets from Zendesk API
@@ -39,6 +52,5 @@ router.get('/ticketsByPage/:pageId', function(req, res){
     res.end(JSON.stringify(tickets.slice(firstIndex, lastIndex)));
   }).catch(console.error); 
 });
-
 
 module.exports = router;
